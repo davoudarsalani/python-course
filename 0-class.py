d083b3e36e4b1082cdbe6fcaa544cecbf8306427
Davoud Arsalani <d_arsalani@yahoo.com>
2024-11-09 20:52:38 +0330

    0-class.py: added Robot class

--- 0-class.py
+++ 0-class.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python
+
+
+'''
+    1. create switches: Cisco, 3com, ...
+    2. inverstigate market
+    3. decide to buy/sell/... the switches
+'''
+
+
+class Robot:
+    ## function inside a class is a 'method'
+    def walk(self):
+        print(f'robot {id(self)} is walking')
+
+    def get_type(self):
+        return type(self)
+
+
+## robot is an object
+## robot is an instance of Robot class
+## this process is called: instantiation
+robot = Robot()
+
+
+## call a method
+robot.walk()
+
+print(robot.get_type())
